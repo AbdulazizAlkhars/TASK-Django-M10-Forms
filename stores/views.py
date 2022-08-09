179dc2f608f7329afc386d4883ffdf75ee19552b
AbdulazizAlkhars <alkharswork@gmail.com>
2022-08-09 20:13:28 +0300

    Update Part Done

--- stores/views.py
+++ stores/views.py
@@ -12,6 +12,19 @@ def get_store_items(request: HttpRequest) -> HttpResponse:
     return render(request, "store_item_list.html", context)
 
 
+def update_store_item(request, item_id):
+    store_item = models.StoreItem.objects.get(id=item_id)
+    form = StoreItemForm(instance=store_item)
+    if request.method == "POST":
+        form = StoreItemForm(request.POST, instance=store_item)
+        if form.is_valid():
+            form.save()
+            return redirect("store-item-list")
+    context = {"form": form,
+               "store_item": store_item}
+    return render(request, "update_store_item.html", context)
+
+
 def create_store_item(request):
     form = StoreItemForm
 
